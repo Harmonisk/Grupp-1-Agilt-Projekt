d3b35185a0eb05adda6502e9c3f04dff978872c8
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-19 13:55:15 +0100

    Initalize Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,22 @@
+//Component: Hero
+//Description:
+//Cecilia vill ha en introduktion till sidan när hon först landar på den så att hon kan få en uppfattning om vad sidan går ut på och få en uppmaning om hur hon ska gå vidare.
+"https://docs.google.com/document/d/1JOHIbR7N138nmvadSs7jaZGplh4xJQa2Xozrw4snhoM/edit?tab=t.0#heading=h.xoy9xlv7gr1w";
+
+import React from "react";
+import Button from "./Button";
+
+export default function Hero() {
+  return (
+    <div className="full-width flex flex-col items-center justify-center bg-amber-600 text-white p-8">
+      <h1>Welcome to this amazing store.</h1>
+      <p>Here you find all things that you almost need.</p>
+      <Button
+        onClick={() => console.log("Button clicked")}
+        className="rounded-md bg-blue-500 text-white"
+      >
+        Come on shooping
+      </Button>
+    </div>
+  );
+}
