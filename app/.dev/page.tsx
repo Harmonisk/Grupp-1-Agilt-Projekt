6b11a870b25cc04e74b3b0b16f2b4226acf73151
Oscar Falk <oscar.falk@gmail.com>
2025-03-19 14:32:15 +0100

    added function and error handling

---
+++ app/.dev/page.tsx
@@ -0,0 +1,14 @@
+import { fetchSingleProduct } from "@/actions/server-actions";
+import Product from "@/interfaces/product";
+import Image from "next/image";
+
+
+export default async function Home() {
+    const response:Product = await fetchSingleProduct(1);
+    return (
+    <>
+      <h2>{}</h2>
+    </>
+    
+  );
+}
