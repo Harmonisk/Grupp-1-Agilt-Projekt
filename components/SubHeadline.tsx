4273d21276baea66cfbfb9c7ee7d59ab8471a640
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-19 14:47:27 +0100

    Description added for component

--- components/SubHeadline.tsx
+++ components/SubHeadline.tsx
@@ -1,3 +1,7 @@
+//Component for the subheadline of the page
+//Description: Som användare vill jag ha en underrubrik för sektioner av sidor
+//så att jag tydligt ser skillnad på sidans sektioner och kan utröna deras syfte.
+
 import React from "react";
 
 export default function SubHeadline({
