f7c208b004a35929e493813577d6484068e0d9dc
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-19 14:25:55 +0100

    Added "use client" becuase of onClick

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,8 @@
 //Cecilia vill ha en introduktion till sidan när hon först landar på den så att hon kan få en uppfattning om vad sidan går ut på och få en uppmaning om hur hon ska gå vidare.
 "https://docs.google.com/document/d/1JOHIbR7N138nmvadSs7jaZGplh4xJQa2Xozrw4snhoM/edit?tab=t.0#heading=h.xoy9xlv7gr1w";
 
+"use client";
+
 import React from "react";
 import Button from "./Button";
 
