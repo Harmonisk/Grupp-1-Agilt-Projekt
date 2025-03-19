ea6306018a385ee278b5fbc5b73bab078ede6e63
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-19 11:37:46 +0100

    Reverted to basic, actually what issue tells.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,61 +1,19 @@
 // Type: Component
 // Description:
-// Can be used to navigate to a different page or trigger an action.
-// It can be used with an icon and a text.
-// It can be used with a href to navigate to a different page or without a href to trigger an action.
-// It can be used with a disabled prop to disable the button.
+// Button component that takes children and an onClick function
 
-import Link from "next/link";
 import React from "react";
 
 type ButtonProps = {
-  href: string;
   children: React.ReactNode;
-  icon?: React.ReactNode;
   className?: string;
-  size?: "small" | "medium" | "large";
   onClick?: () => void;
-  disabled?: boolean;
-  variant?: "primary" | "secondary" | "outline";
 };
-export default function Button({
-  href,
-  icon,
-  children,
-  onClick,
-  size = "medium",
-  disabled = false,
-  variant = "primary",
-}: ButtonProps) {
-  const baseStyles = "rounded-md flex";
-  const sizeStyles = {
-    small: "p-1 text-sm",
-    medium: "p-2 text-base",
-    large: "p-3 text-lg",
-  };
-  const variantStyles = {
-    primary: "bg-blue-500 text-white",
-    secondary: "bg-gray-200 text-gray-800",
-    outline: "border border-gray-800 text-gray-800",
-  };
-
-  // Lets construct our button classes based on the size and variant.
-  const classes = `${baseStyles} ${sizeStyles[size]} ${
-    variantStyles[variant]
-  } ${disabled ? "opacity-50 cursor-not-allowed" : ""} flex-items-center gap-2`;
-
-  // If href is provided, render a Link component with the href.
-  if (href) {
-    return (
-      <Link href={href} className={classes}>
-        {icon}
-        {children}
-      </Link>
-    );
-  }
+export default function Button({ children, onClick }: ButtonProps) {
+  // Base styles for the button
+  const classes = `rounded-md flex flex-items-center gap-2`;
   return (
-    <button onClick={onClick} className={classes} disabled={disabled}>
-      {icon}
+    <button onClick={onClick} className={classes}>
       {children}
     </button>
   );
