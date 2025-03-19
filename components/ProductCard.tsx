2d1653ff0b566a06f1c29a7d50df043213f9385e
jegersterk <jegersterk1@gmail.com>
2025-03-19 14:12:13 +0100

    imported button into component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,12 +1,22 @@
+import Button from "./Button";
 
 export default function ProductCard() {
+	const handleClick = () => {
+		console.log("Button clicked!");
+	  };
+	
 	return (
+		
 		<div className="w-32 h-48 bg-amber-400 rounded-lg overflow-hidden">
 			<div className="h-1/2 bg-amber-400"></div>
 			<div className="h-1/2 bg-blue-400 p-2 flex flex-col justify-between">
-				<h2 className="flex-1 break-words leading-relaxed">CardCardCardCard</h2>
-				<h2 className="flex-1/4 leading-relaxed" >a</h2>
-				<h2 className="flex-1/4 ">b</h2>
+				<h2 className="text-sm leading-4 flex-1 break-words">CardCardCardCard</h2>
+				{/* <h2 className="text-sm leading-4 flex-1 break-words">CardCardCard</h2> */}
+				<div>
+
+				<h2 className="flex-1/4 leading-4" >70 kr</h2>
+				<Button className="bg-amber-700 w-1 h-1 border-2">A</Button>
+				</div>
 			</div>
 		</div>
 	);
