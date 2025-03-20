d70779e719e54b7435a7add0a383fa3a9415bd67
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-20 23:15:44 +0100

    Updated Landing page wirh Main component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@
 import GlobalFooter from "@/components/GlobalFooter";
 import GlobalHeader from "@/components/GlobalHeader";
 import Hero from "@/components/Hero";
+import Main from "@/components/Main";
 import SubHeadline from "@/components/SubHeadline";
 
 export default function Home() {
@@ -12,10 +13,10 @@ export default function Home() {
       <GlobalHeader />
 
       <Hero></Hero>
-      <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Main>
         <SubHeadline>Featured products</SubHeadline>
         {/* <ProductList></ProductList> */}
-      </main>
+      </Main>
 
       <GlobalFooter />
     </>
