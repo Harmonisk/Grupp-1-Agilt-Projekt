dfe2e7fddf519a79fb131cab34ac6fb39b9179ff
jegersterk <jegersterk1@gmail.com>
2025-03-20 18:18:40 +0100

    working on placeholder for loading api. added a domain in next config in order to be able to fetch images from api

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,21 +1,33 @@
-import Button from "./Button";
+'use client'
+import AddToCartButton from "./AddToCartButton";
+import { fetchSingleProduct } from "@/actions/server-actions";
+import React, { useEffect, useState } from "react";
+import Product from "@/interfaces/product";
+import Image from "next/image";
 
-export default function ProductCard() {
-	const handleClick = () => {
-		console.log("Button clicked!");
-	  };
-	
+export default function ProductCard({ id }: { id: number }) {
+	const [product, setProduct] = useState<Product | null>(null);
+	const [imageLoaded, setImageLoaded] = useState(false);
+
+	useEffect(() => {
+		async function getProduct() {
+			const fetchedProduct = await fetchSingleProduct(id);
+			setProduct(fetchedProduct);
+			console.log(fetchedProduct);
+		}
+		getProduct();
+	}, [id]);
+
 	return (
-		
-		<div className="w-32 h-48 bg-amber-400 rounded-lg overflow-hidden">
-			<div className="h-1/2 bg-amber-400"></div>
-			<div className="h-1/2 bg-blue-400 p-2 flex flex-col justify-between">
-				<h2 className="text-sm leading-4 flex-1 break-words">CardCardCardCard</h2>
-				{/* <h2 className="text-sm leading-4 flex-1 break-words">CardCardCard</h2> */}
-				<div>
-
-				<h2 className="flex-1/4 leading-4" >70 kr</h2>
-				<Button className="bg-amber-700 w-1 h-1 border-2">A</Button>
+		<div className="w-48 h-64 bg-white text-black rounded-lg overflow-hidden flex flex-col">
+			<div className="h-4/7 w-auto relative p-5">
+				{product && <Image src={product.images[0]} alt="product" layout="fill" objectFit="cover" />}
+			</div>
+			<div className="h-3/7 bg-[#D9D9D9] p-2 flex flex-col justify-between">
+				<h2 className="text-md font-semibold leading-4 break-words">{product && product.title}</h2>
+				<div className="gap-3">
+					<h2>{product && product.price} kr</h2>
+					<AddToCartButton />
 				</div>
 			</div>
 		</div>
