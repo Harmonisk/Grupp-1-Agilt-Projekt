877466732a5d4df7bc2449e8226d148d9993404f
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-20 23:08:07 +0100

    Enabled globalfooter in page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 //Description:
 
 import GlobalFooter from "@/components/GlobalFooter";
+import GlobalHeader from "@/components/GlobalHeader";
 import Hero from "@/components/Hero";
 import SubHeadline from "@/components/SubHeadline";
 
 export default function Home() {
   return (
     <>
-      {/* <GlobalHeader></GlobalHeader> */}
+      <GlobalHeader />
 
       <Hero></Hero>
       <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
