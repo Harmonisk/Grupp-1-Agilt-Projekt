b404b6e75a400ec802698194a1cbb0eec537ebdb
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-20 23:14:23 +0100

    Add comments and acceptance criteria to Main component

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,8 @@
+//Component for the main content of the page
+//Acceptance Criteria:
+//-The component should contain the main content of the page
+//-The component should be able to contain other components
+
 import React from "react";
 
 export default function Main({ children }: { children: React.ReactNode }) {
