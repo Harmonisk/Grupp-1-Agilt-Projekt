e19f48eb6668c3378f25b059d78006fd6438bc9b
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-20 12:03:53 +0100

    Initiate component

---
+++ components/GlobalHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+export default function GlobalHeader() {
+  return (
+    <div>
+      <div className="logo">Product name</div>
+      {/* <MainMenu></MainMenu> */}
+
+      {/* <Button></Button> */}
+    </div>
+  );
+}
