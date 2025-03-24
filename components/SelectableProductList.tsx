32839b562bf7068cbb5f176cc519d859a4794b09
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-24 13:25:51 +0100

    Add comments

--- components/SelectableProductList.tsx
+++ components/SelectableProductList.tsx
@@ -1,3 +1,6 @@
+// Component: SelectableProductList
+// Desciption: Som användare vill jag kunna filtrera och sortera en lista av produkter
+// så att jag lättare kan hitta de produkter jag är intresserad av.
 import React from "react";
 
 export default function SelectableProductList() {
