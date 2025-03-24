ca11760142fcaecbeee0a76232d7e401dc769619
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-24 13:49:20 +0100

    Init component

--- components/SelectableProductList.tsx
+++ components/SelectableProductList.tsx
@@ -5,12 +5,17 @@ import React from "react";
 import ProductList from "./ProductList";
 import SubHeadline from "./SubHeadline";
 
-export default function SelectableProductList() {
+export default function SelectableProductList({
+  params,
+}: {
+  params: { name: string };
+}) {
   return (
     <div className="selectable-product-list">
-      <SubHeadline>Sub-headline</SubHeadline>
+      {/* params.name would be "Category name" when called from within category/[name]/page.tsx */}
+      <SubHeadline>{params.name}</SubHeadline>
       {/* <FilterSortBar></FilterSortBar> */}
-      <ProductList></ProductList>
+      <ProductList category={params.name}></ProductList>
       {/* <Pagination></Pagination> */}
     </div>
   );
