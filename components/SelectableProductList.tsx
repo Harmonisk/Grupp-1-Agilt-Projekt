5e5cfa9d7a9a5d5523cfa85a226055678bd9cf8f
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-24 13:30:06 +0100

    Init component

--- components/SelectableProductList.tsx
+++ components/SelectableProductList.tsx
@@ -2,7 +2,16 @@
 // Desciption: Som användare vill jag kunna filtrera och sortera en lista av produkter
 // så att jag lättare kan hitta de produkter jag är intresserad av.
 import React from "react";
+import ProductList from "./ProductList";
+import SubHeadline from "./SubHeadline";
 
 export default function SelectableProductList() {
-  return <div>SelectableProductList</div>;
+  return (
+    <div className="selectable-product-list">
+      <SubHeadline>Sub-headline</SubHeadline>
+      {/* <FilterSortBar></FilterSortBar> */}
+      <ProductList></ProductList>
+      {/* <Pagination></Pagination> */}
+    </div>
+  );
 }
