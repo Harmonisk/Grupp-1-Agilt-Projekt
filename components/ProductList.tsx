3cb360f82693b53b89e481fe7863211a2c0923a3
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-24 12:24:46 +0100

    Initiate component

---
+++ components/ProductList.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+import {
+  fetchAllProducts,
+  fetchProductsByCategory,
+} from "@/actions/server-actions";
+import Product from "@/interfaces/product";
+
+interface ProductListProps {
+  category?: string | string[];
+  limit?: number;
+  page?: number;
+  featured?: boolean;
+}
+
+export default async function ProductList({
+  category,
+  limit = 8, // Default values
+  page = 1,
+  featured = false,
+}: ProductListProps) {
+  let products: Product[];
+
+  if (category) {
+    products = await fetchProductsByCategory(category);
+  } else {
+    products = await fetchAllProducts(limit, page, featured);
+  }
+
+  return (
+    <ul className="product-list">
+      {products.map((product) => (
+        <li key={product.id}>
+          {/* {...product} sends all props to ProductCard. */}
+          <ProductCard {...product} />
+        </li>
+      ))}
+    </ul>
+  );
+}
