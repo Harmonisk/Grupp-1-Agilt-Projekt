cf914859fd5374cbcd8112c9de2163da9b4e38df
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-24 16:02:55 +0100

    Add classname and children as prop for component

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,3 +1,6 @@
+// Component: PageHeader
+// Description: Som användare vill jag ha en rubrik för sidan jag är på så att jag enkelt kan tyda syftet för en enskild sida.
+
 import React from "react";
 
 export default function PageHeader({
@@ -6,7 +9,7 @@ export default function PageHeader({
   children: React.ReactNode;
 }) {
   return (
-    <section>
+    <section className="page-header">
       <h1>{children}</h1>
     </section>
   );
