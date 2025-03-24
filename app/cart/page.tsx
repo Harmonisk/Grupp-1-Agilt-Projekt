a33aa3f63e9b7211b94095eae7b136adc1fa863a
Oscar Falk <oscar.falk@gmail.com>
2025-03-24 14:24:12 +0100

    created boilerplate for page

---
+++ app/cart/page.tsx
@@ -0,0 +1,16 @@
+import GlobalFooter from "@/components/GlobalFooter";
+import GlobalHeader from "@/components/GlobalHeader";
+import Main from "@/components/Main";
+
+export default function CartPage(){
+    
+    return(
+        <>
+            <GlobalHeader />
+            <Main>
+                <h2>Cart Page</h2>
+            </Main>
+            <GlobalFooter />
+        </>
+    );
+}
