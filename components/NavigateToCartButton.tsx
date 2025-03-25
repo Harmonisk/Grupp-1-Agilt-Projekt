87ffbd5b9b2f52547e2e10bd1bab52dc2e739179
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-25 11:57:27 +0100

    Init NavigateToCartButton

---
+++ components/NavigateToCartButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+import { useRouter } from "next/router";
+
+export default function NavigateToCartButton() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/checkout");
+  };
+
+  return (
+    <Button onClick={handleClick} className="navigate-to-cart-button">
+      <span className="navigate-to-cart-button__icon"></span>
+      Checkout
+    </Button>
+  );
+}
