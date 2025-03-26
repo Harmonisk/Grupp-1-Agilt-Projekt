b6a48da9a67aff37d96546fcf2d6eea958f5d13a
Peter Laan <plaan70@gmail.com>
2025-03-26 16:28:57 +0100

    Created CartList

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,40 +1,17 @@
-"use client"
 import Main from "@/components/Main";
 import { CartTotal } from "@/components/CartTotal";
-import { useProductContext } from "@/components/ProductContext";
-import Image from "next/image";
-import { RemoveFromCartButton } from "@/components/RemoveFromCartButton";
+import CartList from "@/components/CartList";
+import { ClearButton } from "@/components/ClearButton";
+import Button from "@/components/Button";
 
 export default function CartPage() {
-    const pc = useProductContext()
-
     return (
         <Main>
             <h2>Cart Page</h2>
-            <div><CartTotal /> kr</div>
-            <button>Betala</button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Bild</th>
-                        <th>Vara</th>
-                        <th>Antal</th>
-                        <th>Pris styck</th>
-                        <th>Pris totalt</th>
-                        <th>Ta bort</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {pc.cart.map((p, i) => <tr key={"a" + i}>
-                        <td><Image src={p.images[0]} alt={"Bild på " + p.title} width={100} height={50} /></td>
-                        <td>{p.title}</td>
-                        <td>{p.price}</td>
-                        <td>{p.amount}</td>
-                        <td>{p.amount ?? 0 * p.price}</td>
-                        <td><RemoveFromCartButton product={p} /></td>
-                    </tr>)}
-                </tbody>
-            </table>
+            <CartTotal />
+            <Button>Betala</Button>
+            <ClearButton />
+            <CartList />
         </Main>
     );
 }
