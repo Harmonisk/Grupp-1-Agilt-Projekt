9e55e0e6e4a37eba3a1e64b7ab8e423596879a5e
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-03-27 11:31:15 +0100

    Removed some junk

--- components/ToggleMenuButton.tsx
+++ components/ToggleMenuButton.tsx
@@ -16,7 +16,6 @@ export default function ToggleMenuButton() {
       onClick={handleMenuToggle}
       className="toggle-menu-button"
       data-toggle={menuVisible}
-      alt="Toggle Menu Button"
     >
       <span className="toggle-menu-button__icon"></span>
     </Button>
