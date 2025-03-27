a06205d33a2d9bcb86bbad929b76bca5e789f61b
Oscar Falk <oscar.falk@gmail.com>
2025-03-27 11:29:17 +0100

    switched out PageHeader for header in ProductCategoryPage

--- app/category/[name]/page.tsx
+++ app/category/[name]/page.tsx
@@ -2,7 +2,7 @@ import { fetchAllCategories } from "@/actions/server-actions";
 import GlobalFooter from "@/components/GlobalFooter";
 import GlobalHeader from "@/components/GlobalHeader";
 import Main from "@/components/Main";
-import PageHeader from "@/components/PageHeader";
+import Header from "@/components/Header";
 import SelectableProductList from "@/components/SelectableProductList";
 import Category from "@/interfaces/category";
 
@@ -14,7 +14,7 @@ export default async function ProductCategoryPage({params}:{params: Promise<{nam
     return(
         <>
             <Main>
-                <PageHeader>{category.name}</PageHeader>
+                <Header title={category.name} />
                 <SelectableProductList category={category} />
             </Main>
         </>
